d7a47fad852000350cf1f50bc56ab6486877dffe
Wes Bos <wesbos@gmail.com>
2023-01-01 10:32:57 -0500

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { iconsAsStrings } from './stringify.js';
 export * from 'vscode-icons-js';
 
 export function getSVGStringFromFileType(name) {
-  return iconsAsStrings.find((icon) => icon.name === name).svg;
+  return iconsAsStrings.find((icon) => icon.name === name);
 }
 
 export { iconsAsStrings };
